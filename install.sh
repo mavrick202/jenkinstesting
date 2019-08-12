e71593e28b304ff5ace70856d8e839950d47d846
Mavrick-DevOps <mavrick202@gmail.com>
2019-08-12 11:02:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,3 +13,9 @@ sleep 2s
 echo echo $(date) >> /tmp/folder/file1
 sleep 2s
 echo echo $(date) >> /tmp/folder/file1
+sleep 2s
+echo echo $(date) >> /tmp/folder/file2
+sleep 2s
+echo echo $(date) >> /tmp/folder/file3
+sleep 2s
+echo echo $(date) >> /tmp/folder/file4
